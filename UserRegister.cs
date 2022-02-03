efaecac7dbf07fe8a2469380b2c5a29c25bbcab6
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-02-03 18:45:14 +0530

    test using parameters

---
+++ UserRegister.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingLambda
+{
+    public class UserRegister
+    {
+        //variable
+        private string message;
+
+        public UserRegister(string message)
+        {
+            this.message = message;
+        }
+
+        public string FirstName()
+        {
+            if (this.message.Contains("Happy"))
+                return "Entry Successfull";
+            else
+                return "Entry UnSuccessfull";
+        }
+
+        public string LastName()
+        {
+            if (this.message.Contains("Happy"))
+                return "Entry Successfull";
+            else
+                return "Entry UnSuccessfull";
+        }
+
+        public string Email()
+        {
+            if (this.message.Contains("Happy"))
+                return "Entry Successfull";
+            else
+                return "Entry UnSuccessfull";
+        }
+
+        public string MobileNumber()
+        {
+            if (this.message.Contains("Happy"))
+                return "Entry Successfull";
+            else
+                return "Entry UnSuccessfull";
+        }
+
+        public string Password()
+        {
+            if (this.message.Contains("Happy"))
+                return "Entry Successfull";
+            else
+                return "Entry UnSuccessfull";
+        }
+    }
+}
