410d112b221a4af4e501f82192d7ff9a6d85460e
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-02-03 18:32:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UserRegistration_Lambda;
+
+namespace UserRegistrationUsingLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Reg using Lambda Expressions!");
+            Console.WriteLine("Enter First Name :");
+            string name = Console.ReadLine();
+            //reference fot regex class
+            RegeX regex = new RegeX();
+            //if regex is valid 
+            //it exexutes as valid firstname
+            //otherwise invalid firstname
+            if (regex.ValidFirstName(name))
+                Console.WriteLine("First Name is VALID !!");
+            else
+                Console.WriteLine("First Name is INVALID !!");
+        }
+    }
+}
---
+++ RegeX.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration_Lambda
+{
+    internal class RegeX
+    {
+
+
+        //pattern for valid name
+        public static string REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+        public bool ValidFirstName(string name)
+        {
+            return Regex.IsMatch(name, REGEX);
+        }
+
+    }
+}
