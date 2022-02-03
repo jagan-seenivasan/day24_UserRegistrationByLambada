8584c523816f29ae7378b162e2a4754497a73ef3
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-02-03 18:36:32 +0530

    validate Last name

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UserRegistration_Lambda;
+
+namespace UserRegistrationUsingLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Reg using Lambda Expressions!");
+            Console.WriteLine("Enter First Name :");
+            string name = Console.ReadLine();
+            //reference fot regex class
+            RegeX regex = new RegeX();
+            //if regex is valid 
+            //it exexutes as valid firstname
+            //otherwise invalid firstname
+            if (regex.ValidFirstName(name))
+                Console.WriteLine("First Name is VALID !!");
+            else
+                Console.WriteLine("First Name is INVALID !!");
+
+            Console.WriteLine("Enter Last Name :");
+            //if regex is valid 
+            //it executes as valid lastname
+            //otherwise invalid lastname
+            string lastname = Console.ReadLine();
+            if (regex.ValidLastName(lastname))
+                Console.WriteLine("Last Name is VALID !!");
+            else
+                Console.WriteLine("Last Name is INVALID !!");
+        }
+    }
+}
---
+++ RegeX.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration_Lambda
+{
+    internal class RegeX
+    {
+        //UC-1
+        //pattern for valid name
+        public static string REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+        public bool ValidFirstName(string name)
+        {
+            return Regex.IsMatch(name, REGEX);
+        }
+        //UC-2
+        //pattern for valid name
+        public static string LastNameREGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+        public bool ValidLastName(string name)
+        {
+            return Regex.IsMatch(name, LastNameREGEX);
+        }
+
+    }
+}
