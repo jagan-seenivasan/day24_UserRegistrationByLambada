c1f1022b624e469f5f3966ab94fa6d898c15347c
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-02-03 18:34:32 +0530

    validate First Name

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text.RegularExpressions;
 using UserRegistration_Lambda;
 
@@ -11,12 +11,10 @@ namespace UserRegistrationUsingLambda
             Console.WriteLine("Welcome to User Reg using Lambda Expressions!");
             Console.WriteLine("Enter First Name :");
             string name = Console.ReadLine();
-            //reference fot regex class
+            
             RegeX regex = new RegeX();
-            //if regex is valid 
-            //it exexutes as valid firstname
-            //otherwise invalid firstname
-            if (regex.ValidFirstName(name))
+            
+                        if (regex.ValidFirstName(name))
                 Console.WriteLine("First Name is VALID !!");
             else
                 Console.WriteLine("First Name is INVALID !!");
